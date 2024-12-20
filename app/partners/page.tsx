a067616d27ac8e081d49ce2900b28ddab241eb56
klkvsky <klkvsky@gmail.com>
2024-12-20 20:10:04 +0900

    Changed the animation of team block for mobile breakpoint

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -135,15 +135,15 @@ export default function Home() {
           <motion.div
             initial={{
               opacity: 0,
-              y: -100,
+              x: -100,
             }}
             animate={{
               opacity: 1,
-              y: 0,
+              x: 0,
             }}
             exit={{
               opacity: 0,
-              y: -100,
+              x: -100,
             }}
             transition={{
               bounce: 0
