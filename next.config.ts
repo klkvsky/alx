9031d8afb668e8081c575696bac95a5709e138be
klkvsky <klkvsky@gmail.com>
2024-12-27 19:02:29 +0900

    VOS-45

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  async headers() {
+    return [
+      {
+        source: '/privacy.pdf',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/pdf',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
